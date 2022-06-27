70af39f460d4f7d579d20bbf268d454c2e5091ee
SyntaxEvgen <syntax-evgen@yandex.ru>
2022-06-27 09:56:40 +0300

    Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.  a) Для ввода данных от человека используется элемент TextBox; б) **Реализовать отдельную форму c TextBox для ввода числа.//Старайтесь разбивать программы на подпрограммы. Переписывайте в начало программы условие и свою фамилию. Все программы сделать в одном решении. В свойствах проектах в качестве запускаемого проекта укажите “Текущий выбор”.

---
+++ Lesson7/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Lesson7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textNumb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textNumb
+            // 
+            resources.ApplyResources(this.textNumb, "textNumb");
+            this.textNumb.Name = "textNumb";
+            this.textNumb.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textNumb);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNumb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Lesson7/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace Lesson7
+{ //2. Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток. Компьютер говорит, больше или меньше загаданное число введенного.
+    //a) Для ввода данных от человека используется элемент TextBox;
+    //б) **Реализовать отдельную форму c TextBox для ввода числа.//Старайтесь разбивать программы на подпрограммы. Переписывайте в начало программы условие и свою фамилию. Все программы сделать в одном решении.
+    //В свойствах проектах в качестве запускаемого проекта укажите “Текущий выбор”.
+
+    public enum Result
+    {
+        Угадали,
+        Меньше,
+        Больше,
+        none,
+    }
+
+    public partial class Form1 : Form
+    {
+        public static Func<int, Result> ComparerDel;
+
+        private static int hiddenNumber;
+
+        public static int HiddenNumber
+        {
+            get { return hiddenNumber; }
+            set { hiddenNumber = value; }
+        }
+        /// <summary>
+        /// Кол-во попыток
+        /// </summary>
+        public static int attempts { get; set; }
+
+
+        public static Regex regexnumb;
+        static Form1()
+        {
+            regexnumb = new Regex( "[^0-9]", RegexOptions.Compiled);
+            ComparerDel = ComparerNumber;
+            attempts = 0;
+
+        }
+
+
+        /// <summary>
+        /// Сравнение с тем что загадано
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static Result ComparerNumber(int a)
+        {
+            if(a.Equals(hiddenNumber))
+            {
+                return Result.Угадали;
+            }
+            else if(a > hiddenNumber)
+            {
+                return Result.Больше;
+            }
+            else if (a < hiddenNumber)
+            {
+                return Result.Меньше;
+            }
+            return Result.none;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Угадай число";
+            this.textNumb.KeyPress += CheckInput!;
+            this.textNumb.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+
+
+
+        void CheckInput(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                int.TryParse(textNumb.Text,out var number);                
+                if (ComparerDel != null)
+                {
+                    var res= ComparerDel(number);
+                    MessageBox.Show(res.ToString());
+                    if (res == Result.Угадали)
+                    {
+                        button1.Text = "Играть!";
+                        attempts = 0;
+                        button1.Enabled = true;
+                        textNumb.Enabled = false;
+                        textNumb.Text = "";
+                        MessageBox.Show(res.ToString());
+                        listBox1.Items.Clear();
+                        return;
+                    }
+                    else
+                    {
+                        listBox1.Items.Add($"Кол-во попыток: {++attempts} : {textNumb.Text}");
+                    }
+
+                }            
+            }
+        }
+
+         void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (regexnumb.IsMatch(textNumb.Text))
+            {
+                MessageBox.Show("Введено не число");
+                textNumb.Text = textNumb.Text.Remove(textNumb.Text.Length - 1);
+            }
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListW_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            attempts = 0;
+            button1.Text = "Game start";
+            button1.Enabled = false;
+            textNumb.Enabled = true;
+            listBox1.Items.Clear();
+            Random rnd = new Random();
+            HiddenNumber = rnd.Next(1, 100);
+            this.Text = $"Угадай число, Загадано: {HiddenNumber}";
+        }
+    }
+}
